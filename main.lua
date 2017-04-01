9957c5897a0689ca6e61767d410bf963066a3187
Anonymous <_>
2017-04-01 18:29:31 +0000

    Doubling down on the concept of iteration, since the control points aren't editable anyway

--- main.lua
+++ main.lua
@@ -27,8 +27,6 @@ local function from_basis (v, basis)
 	}
 end
 
-local debug = { 0.0, 0.0 }
-
 local function draw_arc (g, arc, color)
 	if not arc then
 		return
@@ -72,70 +70,10 @@ local function draw_arc (g, arc, color)
 	--g.line (arc.start.pos [1], 0, arc.start.pos [1], 600)
 end
 
-local function solve_circular_arc (start, stop)
-	local start_tangent = { 1.0, 0.0 }
-	local overall_tangent = { stop [1] - start [1], stop [2] - start [2] }
-	local ot_len = math.sqrt (math.pow (overall_tangent [1], 2.0) + math.pow (overall_tangent [2], 2.0))
-	
-	local overall_tangent = {overall_tangent [1] / ot_len, overall_tangent [2] / ot_len}
-	
-	local overall_normal = {-overall_tangent [2], overall_tangent [1]}
-	
-	local start_dot_normal = start_tangent [1] * overall_normal [1] + start_tangent [2] * overall_normal [2]
-	
-	local stop_tangent = { start_tangent [1] - 2 * start_dot_normal * overall_normal [1], start_tangent [2] - 2 * start_dot_normal * overall_normal [2] }
-	
-	local stop_normal = { stop_tangent [2], -stop_tangent [1] }
-	
-	local x_distance = stop [1] - start [1]
-	
-	local epsilon = 0.03125
-	
-	if math.abs (stop_normal [1]) < epsilon and math.abs (x_distance) > epsilon then
-		-- Bail out
-		return stop_normal, { start [1], start [2] }
-	else
-		local t = 0.0
-		if math.abs (stop_normal [1]) >= epsilon then
-			t = x_distance / -stop_normal [1]
-		end
-		local y_intercept = stop [2] + t * stop_normal [2]
-		local radius = y_intercept - start [2]
-		local circumference = radius * 2.0 * math.pi
-		
-		local segment_length = 8.0
-		local segment_radians = segment_length / radius
-		
-		local points = {}
-		
-		-- The first one is just start but like whatever
-		local flip_x = segment_radians < 0.0
-		if flip_x then
-			flip_x = 1.0
-		else
-			flip_x = 1.0
-		end
-		
-		for segment = 0, 16 do
-			local theta = segment * segment_radians
-			
-			table.insert (points, {
-				start [1] + radius * flip_x * math.sin (theta),
-				y_intercept - radius * math.cos (theta),
-			})
-		end
-		
-		return stop_normal, { start [1], y_intercept }, points
-	end
-	
-	return stop_normal
-	--return overall_normal
-end
-
 function bend_arc_basis (start, mouse, basis)
 	local local_mouse = into_basis ({mouse [1] - start [1], mouse [2] - start [2]}, basis)
 	
-	local a, b, c = bend_arc ({0, 0}, local_mouse)
+	local a, c = bend_arc (local_mouse)
 	
 	local lines = {}
 	for i, v in ipairs (c) do
@@ -145,14 +83,14 @@ function bend_arc_basis (start, mouse, basis)
 	
 	local tangent = from_basis (a, basis)
 	
-	return tangent, b, lines
+	return tangent, lines
 end
 
-function bend_arc (start, mouse)
-	local theta = math.atan2 (mouse [2] - start [2], mouse [1] - start [1])
+function bend_arc (mouse)
+	local theta = math.atan2 (mouse [2], mouse [1])
 	local snapped_theta = (math.floor (((theta * 180.0 / math.pi) + 2.5) / 5.0) * 5.0) * math.pi / 180.0
 	
-	local radius = math.sqrt (math.pow (mouse [1] - start [1], 2.0) + math.pow (mouse [2] - start [2], 2.0))
+	local radius = math.sqrt (math.pow (mouse [1], 2.0) + math.pow (mouse [2], 2.0))
 	
 	--[[
 	local snapped_mouse = {
@@ -168,32 +106,30 @@ function bend_arc (start, mouse)
 	local arc_length = radius
 	
 	local curvature = total_arc_theta / arc_length
-	if math.abs (curvature) < 0.0001 then
-		return {1.0, 0.0}, nil, {
-			start,
-			{start [1] + arc_length, start [2]},
+	local lines = tesselate_arc (curvature, arc_length, num_segments)
+	
+	return {math.cos (total_arc_theta), math.sin (total_arc_theta)}, lines
+end
+
+function tesselate_arc (curvature, arc_length, num_segments)
+	local lines = {
+		{0.0, 0.0},
+	}
+	
+	local theta = 0.0
+	local last_point = {0.0, 0.0}
+	local segment_length = arc_length / num_segments
+	for i = 1, num_segments - 1 do
+		theta = theta + curvature * segment_length
+		local point = {
+			last_point [1] + segment_length * math.cos (theta),
+			last_point [2] + segment_length * math.sin (theta),
 		}
-	else 
-		local radius = 1.0 / curvature
-		
-		local center = {
-			start [1],
-			start [2] + radius,
-		}
-		
-		local lines = {}
-		
-		for i = 0, num_segments do
-			local theta = i * total_arc_theta / num_segments - 0.5 * math.pi
-			
-			table.insert (lines, {
-				center [1] + radius * math.cos (theta),
-				center [2] + radius * math.sin (theta),
-			})
-		end
-		
-		return {math.cos (total_arc_theta), math.sin (total_arc_theta)}, nil, lines
+		table.insert (lines, point)
+		last_point = point
 	end
+	
+	return lines
 end
 
 function love.draw ()
@@ -201,7 +137,6 @@ function love.draw ()
 		draw_arc (love.graphics, arc)
 	end
 	draw_arc (love.graphics, arc, {120, 120, 120})
-	--love.graphics.line (debug [1], debug [2], arc.start.pos [1], arc.start.pos [2])
 	
 	if not arc and #arcs == 0 then
 		love.graphics.setColor (120, 120, 120)
@@ -214,7 +149,7 @@ end
 
 function love.mousemoved (x, y)
 	if arc then
-		arc.stop.tangent, debug, arc.lines = bend_arc_basis (arc.start.pos, {x, y}, tangent_to_basis (arc.start.tangent))
+		arc.stop.tangent, arc.lines = bend_arc_basis (arc.start.pos, {x, y}, tangent_to_basis (arc.start.tangent))
 		arc.stop.pos = arc.lines [#arc.lines]
 	end
 	
